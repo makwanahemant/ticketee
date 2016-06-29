bb4eb67ca2a292ac26d12271cc219007a58462c2
hemant@kensvalley.com <hemant@kensvalley.com>
2016-06-29 11:21:15 +0530

    implemented view list of all projects

---
+++ spec/features/viewing_project_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "User can view Projects" do
+  scenario 'with project details' do
+    project = FactoryGirl.create(:project, name: "Sublime Text 3")
+
+    visit '/'
+    click_link "Sublime Text 3"
+    expect(page.current_url).to eq project_url(project)
+  end
+end
