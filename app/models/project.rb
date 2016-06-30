3265dbeb598676cedca3f50c6f7428cbb2aa3816
hemant@kensvalley.com <hemant@kensvalley.com>
2016-06-30 14:20:40 +0530

    Cull tickets when project gets deleted

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ApplicationRecord
-  has_many :tickets
+  has_many :tickets, dependent: :delete_all # :delete_all for fact execution
+  #has_many :tickets, dependent: :destroy # culling the project
+                                         # it will delete all the tickets when
+                                         # project is deleted
   validates :name, presence: true, uniqueness: true
 
 end
